3aacf628397490e65d896321dcef7d76e4b4840d
Andy Cook <acook@Andys-MacBook-Pro.local>
2014-09-19 18:58:48 -0600

    File restructure

--- js/ng/routes.js
+++ js/ng/routes.js
@@ -7,142 +7,142 @@ angular.module('PortalApp.routes', ['ngRoute'])
 .config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.when('/dashboard', {
-            templateUrl: 'partials/dashboard.html',
+            templateUrl: 'js/ng/partials/dashboard.html',
             controller: 'HomeCtrl'
         });
 
         $routeProvider.when('/profile', {
-            templateUrl: 'partials/profile.html'
+            templateUrl: 'js/ng/partials/profile.html'
         });
 
         $routeProvider.when('/flot', {
-            templateUrl: 'partials/flot.html'
+            templateUrl: 'js/ng/partials/flot.html'
         });
 
         $routeProvider.when('/morris', {
-            templateUrl: 'partials/morris.html'
+            templateUrl: 'js/ng/partials/morris.html'
         });
 
         $routeProvider.when('/validation', {
-            templateUrl: 'partials/validation.html'
+            templateUrl: 'js/ng/partials/validation.html'
         });
 
         $routeProvider.when('/basic-form-elements', {
-            templateUrl: 'partials/basic-form-elements.html'
+            templateUrl: 'js/ng/partials/basic-form-elements.html'
         });
 
         $routeProvider.when('/advanced-form-elements', {
-            templateUrl: 'partials/advanced-form-elements.html'
+            templateUrl: 'js/ng/partials/advanced-form-elements.html'
         });
 
         $routeProvider.when('/calendar', {
-            templateUrl: 'partials/calendar.html'
+            templateUrl: 'js/ng/partials/calendar.html'
         });
 
         $routeProvider.when('/wysiwyg-editor', {
-            templateUrl: 'partials/wysiwyg-editor.html'
+            templateUrl: 'js/ng/partials/wysiwyg-editor.html'
         });
 
         $routeProvider.when('/dropzone-uploader', {
-            templateUrl: 'partials/dropzone-uploader.html'
+            templateUrl: 'js/ng/partials/dropzone-uploader.html'
         });
 
         $routeProvider.when('/basic-tables', {
-            templateUrl: 'partials/basic-tables.html'
+            templateUrl: 'js/ng/partials/basic-tables.html'
         });
 
         $routeProvider.when('/advanced-tables', {
-            templateUrl: 'partials/advanced-tables.html'
+            templateUrl: 'js/ng/partials/advanced-tables.html'
         });
 
         $routeProvider.when('/portlets', {
-            templateUrl: 'partials/portlets.html'
+            templateUrl: 'js/ng/partials/portlets.html'
         });
 
         $routeProvider.when('/buttons', {
-            templateUrl: 'partials/buttons.html'
+            templateUrl: 'js/ng/partials/buttons.html'
         });
 
         $routeProvider.when('/tabs-accordions', {
-            templateUrl: 'partials/tabs-accordions.html'
+            templateUrl: 'js/ng/partials/tabs-accordions.html'
         });
 
         $routeProvider.when('/buttons', {
-            templateUrl: 'partials/buttons.html'
+            templateUrl: 'js/ng/partials/buttons.html'
         });
 
         $routeProvider.when('/notifications', {
-            templateUrl: 'partials/notifications.html'
+            templateUrl: 'js/ng/partials/notifications.html'
         });
 
         $routeProvider.when('/sliders', {
-            templateUrl: 'partials/sliders.html'
+            templateUrl: 'js/ng/partials/sliders.html'
         });
 
         $routeProvider.when('/typography', {
-            templateUrl: 'partials/typography.html'
+            templateUrl: 'js/ng/partials/typography.html'
         });
 
         $routeProvider.when('/icons', {
-            templateUrl: 'partials/icons.html'
+            templateUrl: 'js/ng/partials/icons.html'
         });
 
         $routeProvider.when('/grid', {
-            templateUrl: 'partials/grid.html'
+            templateUrl: 'js/ng/partials/grid.html'
         });
 
         $routeProvider.when('/mailbox', {
-            templateUrl: 'partials/mailbox.html'
+            templateUrl: 'js/ng/partials/mailbox.html'
         });
 
         $routeProvider.when('/compose-message', {
-            templateUrl: 'partials/compose-message.html'
+            templateUrl: 'js/ng/partials/compose-message.html'
         });
 
         $routeProvider.when('/message-content', {
-            templateUrl: 'partials/message-content.html'
+            templateUrl: 'js/ng/partials/message-content.html'
         });
 
         $routeProvider.when('/chat', {
-            templateUrl: 'partials/chatBox.html'
+            templateUrl: 'js/ng/partials/chatBox.html'
         });
 
         $routeProvider.when('/faqs', {
-            templateUrl: 'partials/faq.html'
+            templateUrl: 'js/ng/partials/faq.html'
         });
 
         $routeProvider.when('/invoice', {
-            templateUrl: 'partials/invoice.html'
+            templateUrl: 'js/ng/partials/invoice.html'
         });
 
         $routeProvider.when('/faqs', {
-            templateUrl: 'partials/faq.html'
+            templateUrl: 'js/ng/partials/faq.html'
         });
 
         $routeProvider.when('/pricing', {
-            templateUrl: 'partials/pricing.html'
+            templateUrl: 'js/ng/partials/pricing.html'
         });
 
         $routeProvider.when('/search-results', {
-            templateUrl: 'partials/search-results.html'
+            templateUrl: 'js/ng/partials/search-results.html'
         });
 
         $routeProvider.when('/404', {
-            templateUrl: 'partials/404.html'
+            templateUrl: 'js/ng/partials/404.html'
         });
 
         $routeProvider.when('/500', {
-            templateUrl: 'partials/500.html'
+            templateUrl: 'js/ng/partials/500.html'
         });
 
         $routeProvider.when('/account', {
             authRequired: true, // must authenticate before viewing this page
-            templateUrl: 'partials/account.html',
+            templateUrl: 'js/ng/partials/account.html',
             controller: 'AccountCtrl'
         });
 
         $routeProvider.when('/login', {
-            templateUrl: 'partials/login.html',
+            templateUrl: 'js/ng/partials/login.html',
             controller: 'LoginCtrl'
         });
 
